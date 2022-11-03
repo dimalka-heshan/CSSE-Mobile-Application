0f507e07061377f5533cc6e32a318d6acddd280d
Nimna Thiranjaya <nimnathiranjya523@gmail.com>
2022-11-03 15:22:10 +0530

    UI changes

--- src/screens/Driver/DriverQRScanner.jsx
+++ src/screens/Driver/DriverQRScanner.jsx
@@ -7,6 +7,7 @@ import {
   Button,
   TouchableOpacity,
   Alert,
+  Dimensions,
 } from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
 import { useNavigation } from "@react-navigation/core";
@@ -94,39 +95,36 @@ export default function DriverQRScanner() {
   // Return the View
   return (
     <View style={styles.container}>
-
-
-            <View style={styles.rect1}>
-          <View style={styles.group1}>
-            <View style={styles.sIpsum1StackStackRow}>
-              <View style={styles.sIpsum1StackStack}>
-                <View style={styles.sIpsum1Stack}>
-                  <Text style={styles.sIpsum1}></Text>
-                  <FontAwesomeIcon
-                    name="map-pin"
-                    style={styles.icon2}
-                  ></FontAwesomeIcon>
-                  <FontAwesomeIcon
-                    name="location-arrow"
-                    style={styles.icon3}
-                  ></FontAwesomeIcon>
-                </View>
-                <EntypoIcon name="compass" style={styles.icon1}></EntypoIcon>
+      <View style={styles.rect1}>
+        <View style={styles.group1}>
+          <View style={styles.sIpsum1StackStackRow}>
+            <View style={styles.sIpsum1StackStack}>
+              <View style={styles.sIpsum1Stack}>
+                <Text style={styles.sIpsum1}></Text>
+                <FontAwesomeIcon
+                  name="map-pin"
+                  style={styles.icon2}
+                ></FontAwesomeIcon>
+                <FontAwesomeIcon
+                  name="location-arrow"
+                  style={styles.icon3}
+                ></FontAwesomeIcon>
               </View>
-              <View style={styles.loremIpsum1StackColumn}>
-                <View style={styles.loremIpsum1Stack}>
-                  <Text style={styles.loremIpsum1}></Text>
-                  <Text style={styles.stsSriLanka1}>STS - Sri Lanka</Text>
-                </View>
-                <Text style={styles.loremIpsum2}>
-                  Smart Tranceport System - Sri Lanka
-                </Text>
+              <EntypoIcon name="compass" style={styles.icon1}></EntypoIcon>
+            </View>
+            <View style={styles.loremIpsum1StackColumn}>
+              <View style={styles.loremIpsum1Stack}>
+                <Text style={styles.loremIpsum1}></Text>
+                <Text style={styles.stsSriLanka1}>STS - Sri Lanka</Text>
               </View>
+              <Text style={styles.loremIpsum2}>
+                Smart Tranceport System - Sri Lanka
+              </Text>
             </View>
           </View>
-          <Text style={styles.dashboard}>Scan QR</Text>
         </View>
-
+        <Text style={styles.dashboard}>Scan QR</Text>
+      </View>
 
       <View style={styles.barcodebox}>
         <BarCodeScanner
@@ -138,57 +136,61 @@ export default function DriverQRScanner() {
       <Text style={styles.qrTextStatus}>{text}</Text>
       {scanned && (
         <View>
-          <Button
+          {/* <Button
             title={"Scan again?"}
             onPress={handleScanAgain}
             color="tomato"
-          />
+          /> */}
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              onPress={UserGetonBtnClick}
+              style={[styles.button, styles.buttonOutliner]}
+            >
+              <Text style={styles.buttonText}>Get on the Bus</Text>
+            </TouchableOpacity>
 
-
-            </View>
-
+            <TouchableOpacity
+              onPress={userGetoff}
+              style={[styles.button, styles.buttonOutliner]}
+            >
+              <Text style={styles.buttonOutlineText}>Get off the Bus</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       )}
-                <View style={styles.buttonContainer}>
-                <TouchableOpacity
-                  onPress={UserGetonBtnClick}
-                  style={[styles.button, styles.buttonOutliner]}
-                >
-                  <Text style={styles.buttonText}>Get on the Bus</Text>
-                </TouchableOpacity>
 
-                <TouchableOpacity
-                  onPress={userGetoff}
-                  style={[styles.button, styles.buttonOutliner]}
-                >
-                  <Text style={styles.buttonOutlineText}>Get off the Bus</Text>
-                </TouchableOpacity>
-              </View>
-
-
-<View style={[styles.container1, styles.cupertinoFooter1]}>
-      <TouchableOpacity style={styles.btnWrapper1} onPress={() => navigation.navigate('BusProfiles')}>
-        <MaterialCommunityIconsIcon
-          name="bus-side"
-          style={styles.icon00}
-        ></MaterialCommunityIconsIcon>
-        <Text style={styles.profile11}>Bus</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.btnWrapper2} onPress={() => navigation.navigate('DriverQRScanner')}>
-        <MaterialCommunityIconsIcon
-          name="qrcode-scan"
-          style={styles.icon100}
-        ></MaterialCommunityIconsIcon>
-        <Text style={styles.scanQr}>Scan QR</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.btnWrapper4} onPress={() => navigation.navigate('Driverprofiles')}>
-        <MaterialCommunityIconsIcon
-          name="account"
-          style={styles.icon300}
-        ></MaterialCommunityIconsIcon>
-        <Text style={styles.profile}>Profile</Text>
-      </TouchableOpacity>
-    </View>
-
+      <View style={[styles.container1, styles.cupertinoFooter1]}>
+        <TouchableOpacity
+          style={styles.btnWrapper1}
+          onPress={() => navigation.navigate("BusProfiles")}
+        >
+          <MaterialCommunityIconsIcon
+            name="bus-side"
+            style={styles.icon00}
+          ></MaterialCommunityIconsIcon>
+          <Text style={styles.profile11}>Bus</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.btnWrapper2}
+          onPress={() => navigation.navigate("DriverQRScanner")}
+        >
+          <MaterialCommunityIconsIcon
+            name="qrcode-scan"
+            style={styles.icon100}
+          ></MaterialCommunityIconsIcon>
+          <Text style={styles.scanQr}>Scan QR</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.btnWrapper4}
+          onPress={() => navigation.navigate("Driverprofiles")}
+        >
+          <MaterialCommunityIconsIcon
+            name="account"
+            style={styles.icon300}
+          ></MaterialCommunityIconsIcon>
+          <Text style={styles.profile}>Profile</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -199,6 +201,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    height: Dimensions.get("window").height,
+    width: Dimensions.get("window").width,
   },
   maintext: {
     marginTop: 20,
@@ -224,7 +228,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#FFBC26",
     borderColor: "#FFBC26",
     borderWidth: 4,
-    marginTop: 105
+    marginTop: 105,
   },
   buttonContainer: {
     width: "60%",
@@ -255,107 +259,99 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
 
-
-
-
-
-
-
-
-
   group2: {
     width: 393,
-    height: 851
+    height: 851,
   },
   rect1: {
-    width: 393,
+    width: Dimensions.get("window").width,
     height: 167,
-    top:-20,
-    position:"absolute",
-    backgroundColor: "rgba(0,0,0,1)"
+    top: 0,
+    position: "absolute",
+    backgroundColor: "rgba(0,0,0,1)",
   },
   group1: {
-    width: 243,
+    width: Dimensions.get("window").width,
     height: 55,
     marginTop: 56,
-    marginLeft: 75
+    marginLeft: 75,
   },
   sIpsum1: {
     top: 23,
     left: 24,
     position: "absolute",
-    color: "#121212"
+    color: "#121212",
   },
   icon2: {
     top: 0,
     left: 11,
     position: "absolute",
     color: "rgba(208,2,27,1)",
-    fontSize: 27
+    fontSize: 27,
   },
   icon3: {
     top: 23,
     left: 0,
     position: "absolute",
     color: "rgba(74,144,226,1)",
-    fontSize: 34
+    fontSize: 34,
   },
   sIpsum1Stack: {
     top: 0,
     left: 19,
     width: 27,
     height: 57,
-    position: "absolute"
+    position: "absolute",
   },
   icon1: {
     top: 6,
     left: 0,
     position: "absolute",
     color: "rgba(255,255,255,1)",
-    fontSize: 30
+    fontSize: 30,
   },
   sIpsum1StackStack: {
     width: 46,
-    height: 57
+    height: 57,
   },
   loremIpsum1: {
     top: 13,
     left: 13,
     position: "absolute",
-    color: "#121212"
+    color: "#121212",
   },
   stsSriLanka1: {
     top: 0,
     left: 0,
     position: "absolute",
     color: "rgba(255,255,255,1)",
-    fontSize: 21
+    fontSize: 21,
   },
   loremIpsum1Stack: {
     width: 140,
     height: 26,
-    marginLeft: 12
+    marginLeft: 12,
   },
   loremIpsum2: {
     width: 1000,
     color: "rgba(255,255,255,1)",
-    fontSize: 12
+    fontSize: 12,
   },
   loremIpsum1StackColumn: {
     width: 195,
     marginLeft: 2,
     marginTop: 13,
-    marginBottom: 4
+    marginBottom: 4,
   },
   sIpsum1StackStackRow: {
     height: 57,
-    flexDirection: "row"
+    flexDirection: "row",
   },
   dashboard: {
     color: "rgba(255,255,255,1)",
     fontSize: 25,
     marginTop: 20,
-    marginLeft: 9
+    marginLeft: 9,
   },
   rect2: {
     top: 7,
@@ -368,30 +364,30 @@ const styles = StyleSheet.create({
     shadowColor: "rgba(155,155,155,1)",
     shadowOffset: {
       width: 3,
-      height: 3
+      height: 3,
     },
     elevation: 45,
     shadowOpacity: 1,
-    shadowRadius: 15
+    shadowRadius: 15,
   },
   driverProfile: {
     color: "#121212",
     fontSize: 18,
     marginTop: 58,
-    marginLeft: 32
+    marginLeft: 32,
   },
   image: {
     top: 0,
     left: 168,
     width: 188,
     height: 145,
-    position: "absolute"
+    position: "absolute",
   },
   rect2Stack: {
     width: 356,
     height: 145,
     marginTop: 49,
-    marginLeft: 23
+    marginLeft: 23,
   },
   rect4: {
     top: 0,
@@ -404,30 +400,30 @@ const styles = StyleSheet.create({
     shadowColor: "rgba(155,155,155,1)",
     shadowOffset: {
       width: 3,
-      height: 3
+      height: 3,
     },
     elevation: 45,
     shadowOpacity: 1,
-    shadowRadius: 15
+    shadowRadius: 15,
   },
   bus: {
     color: "#121212",
     fontSize: 18,
     marginTop: 55,
-    marginLeft: 37
+    marginLeft: 37,
   },
   image2: {
     top: 0,
     left: 126,
     width: 236,
     height: 132,
-    position: "absolute"
+    position: "absolute",
   },
   rect4Stack: {
     width: 362,
     height: 132,
     marginTop: 29,
-    marginLeft: 23
+    marginLeft: 23,
   },
   rect3: {
     top: 0,
@@ -440,103 +436,100 @@ const styles = StyleSheet.create({
     shadowColor: "rgba(155,155,155,1)",
     shadowOffset: {
       width: 3,
-      height: 3
+      height: 3,
     },
     elevation: 45,
     shadowOpacity: 1,
-    shadowRadius: 15
+    shadowRadius: 15,
   },
   scanPassengerQr: {
     color: "#121212",
     fontSize: 18,
     marginTop: 53,
-    marginLeft: 32
+    marginLeft: 32,
   },
   image3: {
     top: 0,
     left: 165,
     width: 194,
     height: 144,
-    position: "absolute"
+    position: "absolute",
   },
   rect3Stack: {
     width: 359,
     height: 144,
     marginTop: 32,
-    marginLeft: 23
+    marginLeft: 23,
   },
   cupertinoFooter1: {
-    height: 55,
-    width: 393,
+    height: 58,
+    width: Dimensions.get("window").width,
     backgroundColor: "rgba(0,0,0,1)",
     position: "absolute",
-    bottom: -4
+    bottom: -4,
   },
-
-
-
 
   container1: {
     backgroundColor: "rgba(255,255,255,1)",
     flexDirection: "row",
-    width: "100%"
+    width: Dimensions.get("window").width,
   },
   btnWrapper1: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
   },
   icon00: {
     backgroundColor: "transparent",
     color: "rgba(255,255,255,1)",
     fontSize: 39,
     marginTop: -5,
-    opacity: 0.8
+    opacity: 0.8,
   },
   bus1: {
     fontSize: 12,
     backgroundColor: "transparent",
-    paddingTop: 4
+    paddingTop: 4,
   },
   btnWrapper2: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
   },
   icon100: {
     backgroundColor: "transparent",
     color: "rgba(255,255,255,1)",
     fontSize: 24,
-    opacity: 0.8
+    opacity: 0.8,
   },
   scanQr: {
     fontSize: 12,
     color: "rgba(255,255,255,1)",
     backgroundColor: "transparent",
-    paddingTop: 4
+    paddingTop: 4,
   },
   btnWrapper4: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
   },
   icon300: {
     backgroundColor: "transparent",
     color: "rgba(255,255,255,1)",
     fontSize: 24,
-    opacity: 0.8
+    opacity: 0.8,
   },
   profile: {
     fontSize: 12,
     color: "rgba(255,255,255,1)",
     backgroundColor: "transparent",
-    paddingTop: 4
+    paddingTop: 4,
   },
   profile11: {
     fontSize: 12,
     color: "rgba(255,255,255,1)",
     backgroundColor: "transparent",
     marginTop: -10,
-    paddingTop: 4
-  }
+    paddingTop: 4,
+  },
 });
--- src/screens/Driver/PassengerGetOnScreen.jsx
+++ src/screens/Driver/PassengerGetOnScreen.jsx
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, View, Text, TouchableOpacity, Alert } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  Alert,
+  Dimensions,
+} from "react-native";
 import { Avatar, Card, Title, Paragraph } from "react-native-paper";
 
 import { Dropdown } from "react-native-element-dropdown";
@@ -21,7 +28,6 @@ const PassengerGetOnScreen = () => {
   const [travelCost, settravelCost] = useState(null);
 
   const route = useRoute();
-
 
   useEffect(() => {
     const getHolts = async () => {
@@ -48,11 +54,6 @@ const PassengerGetOnScreen = () => {
     };
     getHolts();
   }, []);
-
-  //console.log(data);
-
-  // console.log("getOnBusHolt : ", getOnBusHolt);
-  // console.log("getOffBusHolt : ", getOffBusHolt);
 
   const renderItem = (item) => {
     return (
@@ -110,38 +111,36 @@ const PassengerGetOnScreen = () => {
   };
   return (
     <View style={styles.container}>
-
-<View style={styles.rect1}>
-          <View style={styles.group1}>
-            <View style={styles.sIpsum1StackStackRow}>
-              <View style={styles.sIpsum1StackStack}>
-                <View style={styles.sIpsum1Stack}>
-                  <Text style={styles.sIpsum1}></Text>
-                  <FontAwesomeIcon
-                    name="map-pin"
-                    style={styles.icon2}
-                  ></FontAwesomeIcon>
-                  <FontAwesomeIcon
-                    name="location-arrow"
-                    style={styles.icon3}
-                  ></FontAwesomeIcon>
-                </View>
-                <EntypoIcon name="compass" style={styles.icon1}></EntypoIcon>
+      <View style={styles.rect1}>
+        <View style={styles.group1}>
+          <View style={styles.sIpsum1StackStackRow}>
+            <View style={styles.sIpsum1StackStack}>
+              <View style={styles.sIpsum1Stack}>
+                <Text style={styles.sIpsum1}></Text>
+                <FontAwesomeIcon
+                  name="map-pin"
+                  style={styles.icon2}
+                ></FontAwesomeIcon>
+                <FontAwesomeIcon
+                  name="location-arrow"
+                  style={styles.icon3}
+                ></FontAwesomeIcon>
               </View>
-              <View style={styles.loremIpsum1StackColumn}>
-                <View style={styles.loremIpsum1Stack}>
-                  <Text style={styles.loremIpsum1}></Text>
-                  <Text style={styles.stsSriLanka1}>STS - Sri Lanka</Text>
-                </View>
-                <Text style={styles.loremIpsum2}>
-                  Smart Tranceport System - Sri Lanka
-                </Text>
+              <EntypoIcon name="compass" style={styles.icon1}></EntypoIcon>
+            </View>
+            <View style={styles.loremIpsum1StackColumn}>
+              <View style={styles.loremIpsum1Stack}>
+                <Text style={styles.loremIpsum1}></Text>
+                <Text style={styles.stsSriLanka1}>STS - Sri Lanka</Text>
               </View>
+              <Text style={styles.loremIpsum2}>
+                Smart Tranceport System - Sri Lanka
+              </Text>
             </View>
           </View>
-          <Text style={styles.dashboard}>Travel Details</Text>
         </View>
-
+        <Text style={styles.dashboard}>Travel Details</Text>
+      </View>
 
       <Text>{route.params.QRdata}</Text>
       <Card style={styles.card}>
@@ -246,31 +245,38 @@ const PassengerGetOnScreen = () => {
         </View>
       ) : null}
 
-
-<View style={[styles.container1, styles.cupertinoFooter1]}>
-      <TouchableOpacity style={styles.btnWrapper1} onPress={() => navigation.navigate('BusProfiles')}>
-        <MaterialCommunityIconsIcon
-          name="bus-side"
-          style={styles.icon00}
-        ></MaterialCommunityIconsIcon>
-        <Text style={styles.profile11}>Bus</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.btnWrapper2} onPress={() => navigation.navigate('DriverQRScanner')}>
-        <MaterialCommunityIconsIcon
-          name="qrcode-scan"
-          style={styles.icon100}
-        ></MaterialCommunityIconsIcon>
-        <Text style={styles.scanQr}>Scan QR</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.btnWrapper4} onPress={() => navigation.navigate('Driverprofiles')}>
-        <MaterialCommunityIconsIcon
-          name="account"
-          style={styles.icon300}
-        ></MaterialCommunityIconsIcon>
-        <Text style={styles.profile}>Profile</Text>
-      </TouchableOpacity>
-    </View>
-
+      <View style={[styles.container1, styles.cupertinoFooter1]}>
+        <TouchableOpacity
+          style={styles.btnWrapper1}
+          onPress={() => navigation.navigate("BusProfiles")}
+        >
+          <MaterialCommunityIconsIcon
+            name="bus-side"
+            style={styles.icon00}
+          ></MaterialCommunityIconsIcon>
+          <Text style={styles.profile11}>Bus</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.btnWrapper2}
+          onPress={() => navigation.navigate("DriverQRScanner")}
+        >
+          <MaterialCommunityIconsIcon
+            name="qrcode-scan"
+            style={styles.icon100}
+          ></MaterialCommunityIconsIcon>
+          <Text style={styles.scanQr}>Scan QR</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.btnWrapper4}
+          onPress={() => navigation.navigate("Driverprofiles")}
+        >
+          <MaterialCommunityIconsIcon
+            name="account"
+            style={styles.icon300}
+          ></MaterialCommunityIconsIcon>
+          <Text style={styles.profile}>Profile</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -379,96 +385,96 @@ const styles = StyleSheet.create({
   },
   group2: {
     width: 393,
-    height: 851
+    height: 851,
   },
   rect1: {
-    width: 393,
+    width: Dimensions.get("window").width,
     height: 167,
-    top:0,
-    backgroundColor: "rgba(0,0,0,1)"
+    top: 0,
+    backgroundColor: "rgba(0,0,0,1)",
   },
   group1: {
     width: 243,
     height: 55,
     marginTop: 56,
-    marginLeft: 75
+    marginLeft: 75,
   },
   sIpsum1: {
     top: 23,
     left: 24,
     position: "absolute",
-    color: "#121212"
+    color: "#121212",
   },
   icon2: {
     top: 0,
     left: 11,
     position: "absolute",
     color: "rgba(208,2,27,1)",
-    fontSize: 27
+    fontSize: 27,
   },
   icon3: {
     top: 23,
     left: 0,
     position: "absolute",
     color: "rgba(74,144,226,1)",
-    fontSize: 34
+    fontSize: 34,
   },
   sIpsum1Stack: {
     top: 0,
     left: 19,
     width: 27,
     height: 57,
-    position: "absolute"
+    position: "absolute",
   },
   icon1: {
     top: 6,
     left: 0,
     position: "absolute",
     color: "rgba(255,255,255,1)",
-    fontSize: 30
+    fontSize: 30,
   },
   sIpsum1StackStack: {
     width: 46,
-    height: 57
+    height: 57,
   },
   loremIpsum1: {
     top: 13,
     left: 13,
     position: "absolute",
-    color: "#121212"
+    color: "#121212",
   },
   stsSriLanka1: {
     top: 0,
     left: 0,
     position: "absolute",
     color: "rgba(255,255,255,1)",
-    fontSize: 21
+    fontSize: 21,
   },
   loremIpsum1Stack: {
     width: 140,
     height: 26,
-    marginLeft: 12
+    marginLeft: 12,
   },
   loremIpsum2: {
     width: 1000,
     color: "rgba(255,255,255,1)",
-    fontSize: 12
+    fontSize: 12,
   },
   loremIpsum1StackColumn: {
     width: 195,
     marginLeft: 2,
     marginTop: 13,
-    marginBottom: 4
+    marginBottom: 4,
   },
   sIpsum1StackStackRow: {
     height: 57,
-    flexDirection: "row"
+    flexDirection: "row",
   },
   dashboard: {
     color: "rgba(255,255,255,1)",
     fontSize: 25,
     marginTop: 20,
-    marginLeft: 9
+    marginLeft: 9,
   },
   rect2: {
     top: 7,
@@ -481,30 +487,30 @@ const styles = StyleSheet.create({
     shadowColor: "rgba(155,155,155,1)",
     shadowOffset: {
       width: 3,
-      height: 3
+      height: 3,
     },
     elevation: 45,
     shadowOpacity: 1,
-    shadowRadius: 15
+    shadowRadius: 15,
   },
   driverProfile: {
     color: "#121212",
     fontSize: 18,
     marginTop: 58,
-    marginLeft: 32
+    marginLeft: 32,
   },
   image: {
     top: 0,
     left: 168,
     width: 188,
     height: 145,
-    position: "absolute"
+    position: "absolute",
   },
   rect2Stack: {
     width: 356,
     height: 145,
     marginTop: 49,
-    marginLeft: 23
+    marginLeft: 23,
   },
   rect4: {
     top: 0,
@@ -517,30 +523,30 @@ const styles = StyleSheet.create({
     shadowColor: "rgba(155,155,155,1)",
     shadowOffset: {
       width: 3,
-      height: 3
+      height: 3,
     },
     elevation: 45,
     shadowOpacity: 1,
-    shadowRadius: 15
+    shadowRadius: 15,
   },
   bus: {
     color: "#121212",
     fontSize: 18,
     marginTop: 55,
-    marginLeft: 37
+    marginLeft: 37,
   },
   image2: {
     top: 0,
     left: 126,
     width: 236,
     height: 132,
-    position: "absolute"
+    position: "absolute",
   },
   rect4Stack: {
     width: 362,
     height: 132,
     marginTop: 29,
-    marginLeft: 23
+    marginLeft: 23,
   },
   rect3: {
     top: 0,
@@ -553,103 +559,100 @@ const styles = StyleSheet.create({
     shadowColor: "rgba(155,155,155,1)",
     shadowOffset: {
       width: 3,
-      height: 3
+      height: 3,
     },
     elevation: 45,
     shadowOpacity: 1,
-    shadowRadius: 15
+    shadowRadius: 15,
   },
   scanPassengerQr: {
     color: "#121212",
     fontSize: 18,
     marginTop: 53,
-    marginLeft: 32
+    marginLeft: 32,
   },
   image3: {
     top: 0,
     left: 165,
     width: 194,
     height: 144,
-    position: "absolute"
+    position: "absolute",
   },
   rect3Stack: {
     width: 359,
     height: 144,
     marginTop: 32,
-    marginLeft: 23
+    marginLeft: 23,
   },
   cupertinoFooter1: {
-    height: 55,
-    width: 393,
+    height: 58,
+    width: Dimensions.get("window").width,
     backgroundColor: "rgba(0,0,0,1)",
     position: "absolute",
-    marginTop: 750
+    marginTop: Dimensions.get("window").height - 56,
   },
-
-
-
 
   container1: {
     backgroundColor: "rgba(255,255,255,1)",
     flexDirection: "row",
-    width: "100%"
+    width: "100%",
   },
   btnWrapper1: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
   },
   icon00: {
     backgroundColor: "transparent",
     color: "rgba(255,255,255,1)",
     fontSize: 39,
     marginTop: -5,
-    opacity: 0.8
+    opacity: 0.8,
   },
   bus1: {
     fontSize: 12,
     backgroundColor: "transparent",
-    paddingTop: 4
+    paddingTop: 4,
   },
   btnWrapper2: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
   },
   icon100: {
     backgroundColor: "transparent",
     color: "rgba(255,255,255,1)",
     fontSize: 24,
-    opacity: 0.8
+    opacity: 0.8,
   },
   scanQr: {
     fontSize: 12,
     color: "rgba(255,255,255,1)",
     backgroundColor: "transparent",
-    paddingTop: 4
+    paddingTop: 4,
   },
   btnWrapper4: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
   },
   icon300: {
     backgroundColor: "transparent",
     color: "rgba(255,255,255,1)",
     fontSize: 24,
-    opacity: 0.8
+    opacity: 0.8,
   },
   profile: {
     fontSize: 12,
     color: "rgba(255,255,255,1)",
     backgroundColor: "transparent",
-    paddingTop: 4
+    paddingTop: 4,
   },
   profile11: {
     fontSize: 12,
     color: "rgba(255,255,255,1)",
     backgroundColor: "transparent",
     marginTop: -10,
-    paddingTop: 4
-  }
+    paddingTop: 4,
+  },
 });
